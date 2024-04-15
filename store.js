e8f3a52a80687ec305b84674a64b480bf58d5140
mjfarrelm <matthewfarrelm@googlemail.com>
2024-04-15 13:56:19 +0100

    first commit

---
+++ store.js
@@ -0,0 +1,10 @@
+//TO-DO LIST
+//1- Populate the store page with the information from Items.JSON
+//i- access the class/ dataset of the relevant elements
+//ii- connect them to the relevant piece of data held in the JSON file
+//iii- use JSON stringify to convert language into JS
+
+//2- Set up the functionality of the shopping cart
+//i- have cart disappear if there are no items added
+//ii- have cart track each click on 'add to cart'
+//iii-
